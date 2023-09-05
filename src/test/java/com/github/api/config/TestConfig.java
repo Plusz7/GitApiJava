a7298f186e9da48cc6190bf64a25b5c6752fb049
Plusz7 <darek.pluszczynski@gmail.com>
2023-09-05 18:19:45 +0200

    adjustments to integration test

--- src/test/java/com/github/api/config/TestConfig.java
+++ src/test/java/com/github/api/config/TestConfig.java
@@ -1,11 +1,8 @@
 package com.github.api.config;
 
-import com.github.api.repository.GitHubRepositoryTest;
 import okhttp3.mockwebserver.MockWebServer;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @TestConfiguration
